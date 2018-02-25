3e8575821eeef7200356c1139b27f3d11576883e
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-02-25 23:27:33 +0300

    little fix for numbers containing more than one 'max'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ module.exports = function getZerosCount(number, base) {
     // your implementation
     var result = 0; //количество нулей в конце
     var max; //максимальный делитель числа
-    var nat = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]; //список натуральных чисел от 2 до 256
+    var aux_number = base;
+    var nat = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]; //список простых чисел от 2 до 256
     for(var i = 0; i < nat.length; i++){
         if(base % nat[i] == 0){
             max = nat[i];
@@ -11,10 +12,18 @@ module.exports = function getZerosCount(number, base) {
             break;
         }
     }
-    var aux_number = 1;
+    var max_count = 0;
+    while(aux_number % max == 0){
+        max_count++;
+        aux_number /= max;
+    }
+    //console.log("max:" + max + ",max_count:"+max_count);
+    aux_number = 1;
     while(aux_number * max <= number){
         aux_number *= max;
         result += Math.floor(number / aux_number);
     }
+    result = Math.floor(result / max_count);
+    //console.log(result);
     return result;
 }
