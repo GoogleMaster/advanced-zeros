8ebf0a8f80874d7b06802fc4b7a697ea1ffa186c
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-03-09 09:53:27 +0300

    100/100

--- src/index.js
+++ src/index.js
@@ -1,71 +1,41 @@
 module.exports = function getZerosCount(number, base) {
     // your implementation
-    function countMinNFact(n,m,min){
-        let count = 0;
-        min = min || 1;
-        for(let i = min; i <= n; i++){
-            let num = i;
-            while(num % m == 0){
-                count++;
-                num /= m;
-            }
-        }
-        return count;
-    }
+    var nat = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]; //список простых чисел от 2 до 256
+    var dividers = [];
+    var dividersNumber = [];
+
     function countMinN(n,m){
-        let count = 0;
         while(n % m == 0){
-            n /= m;
+            var count = count || 0;
             count++;
+            n /= m;
         }
         return count;
     }
 
-    var result = 0; //количество нулей в конце
-    var max; //максимальный делитель числа
-    var aux_number = base;
-    var nat = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]; //список простых чисел от 2 до 256
-    var dividers = [];
-    for(var i = 0; i < nat.length; i++){
+    function countMinNFact(n,m){
+        var count = 0;
+        /*for(let i = m; i <= n; i += m){
+            count += countMinN(i,m);
+        }*/
+        for(let i = m; i <= number; i *= m){
+            //if(m > n) break;
+            count += Math.floor(n / i);
+        }
+        return count;
+    }
+
+    for(let i = 0; i < nat.length; i++){
         if(base % nat[i] == 0){
-            max = nat[i];
             dividers.push([nat[i],countMinN(base,nat[i])]);
         }
-        if(nat[i] > base){
-            break;
-        }
+        if(nat[i+1] > base) break;
     }
-    var max_count = 0;
-    /*while(aux_number % max == 0){
-        max_count++;
-        aux_number /= max;
+
+    for(let i = 0; i < dividers.length; i++){
+        dividers[i] = [dividers[i][0],dividers[i][1],Math.floor(countMinNFact(number,dividers[i][0])/dividers[i][1])];
     }
-    //console.log(max_count);
-    if(max_count == 0){
-        while(aux_number / max > max){
-            max_count++;
-            aux_number /= max;
-        }
-    }*/
-    if(dividers.length >= 2 && dividers[0][1] / dividers[dividers.length - 1][1] >= dividers[dividers.length-1][0] * dividers[0][0]){
-        while(aux_number / max > max){
-            max_count++;
-            aux_number /= max;
-        }
-    }else{
-        while(aux_number % max == 0){
-            max_count++;
-            aux_number /= max;
-        }
-    }
-    //console.log("max:" + max + ",max_count:"+max_count);
-    aux_number = 1;
-    while(aux_number * max <= number){
-        aux_number *= max;
-        result += Math.floor(number / aux_number);
-    }
-    //console.log(result);
-    result = Math.floor(result / max_count);
-    console.log(result);
-    return result;
+    dividers.sort(function(a,b){return a[2]-b[2];});
+
+    return dividers[0][2];
 }
