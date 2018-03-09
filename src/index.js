5f97a97b4a49a4f0595f08dbe06ff920310080a8
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-03-09 09:56:10 +0300

    comments and useless code were deleted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ module.exports = function getZerosCount(number, base) {
     // your implementation
     var nat = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]; //список простых чисел от 2 до 256
     var dividers = [];
-    var dividersNumber = [];
 
     function countMinN(n,m){
         while(n % m == 0){
@@ -15,11 +14,7 @@ module.exports = function getZerosCount(number, base) {
 
     function countMinNFact(n,m){
         var count = 0;
-        /*for(let i = m; i <= n; i += m){
-            count += countMinN(i,m);
-        }*/
         for(let i = m; i <= number; i *= m){
-            //if(m > n) break;
             count += Math.floor(n / i);
         }
         return count;
